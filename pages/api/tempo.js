3aa1b491d6dceb983e616242823d0b80f6ee4846
Nikolas Victor Mota <nikolasVictorTola@gmail.com>
2024-05-13 18:37:11 -0300

    Resolvendo problema de CORS

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,25 +2,15 @@ var express = require('express')
 var cors = require('cors')
 var app = express()
  
-var whitelist = 'https://orange-wall.vercel.app/';
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+app.use(cors())
  
-app.get('/api/tempo', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for a whitelisted domain.'})
+app.get('/tempo', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
 })
  
 app.listen(80, function () {
   console.log('CORS-enabled web server listening on port 80')
 })
-
 //-----------------------------
 
 function tempo(request, response) {
