6d348589de361e9cc604fefb9277cf3b4bbe739a
Nikolas Victor Mota <nikolasVictorTola@gmail.com>
2024-05-13 19:20:29 -0300

    Resolvendo problema de CORS

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,21 +1,3 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
-console.log('teste');
- 
-console.log(cors);
-console.log(cors.origin)
-app.use((req, res, next) =>{
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET', 'PUT');
-  console.log(req.header('origin'));
-
-  app.use(cors());
-
-  next();
-});
-//-----------------------------
-
 function tempo(request, response) {
       const dynamicDate = new Date();
 
