6cdae4b0ddaeae24e9485d483d7d45a8d3be9b3b
Nikolas Victor Mota <nikolasVictorTola@gmail.com>
2024-05-13 20:18:05 -0300

    Buscando API da steam

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -11,7 +11,7 @@ async function tempo(request, response) {
 
       const steamResponse = await fetch("https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=2627FEB2EFFCED8437F659EB577291EE&steamids=76561198841564068");
       const steamResponseJSON = await steamResponse.json();
-      const nome = steamResponse.response.players.personaname;
+      const nome = steamResponseJSON.response.players;
 
       response.json({
         date: dynamicDate.toGMTString(),
