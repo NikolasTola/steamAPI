bef92263c457ccd057fa6104fe10e6b66d6fe66c
Nikolas Victor Mota <nikolasVictorTola@gmail.com>
2024-05-13 19:34:06 -0300

    Resolvendo problema de CORS

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,3 +1,10 @@
+const cors = require('cors');
+
+cors.header("Access-Control-Allow-Origin", "*");
+cors.header("Access-Control-Allow-Methods", "GET");
+
+cors();
+console.log('TESTE');
 function tempo(request, response) {
       const dynamicDate = new Date();
 
