3fe8738b476930364a09d62e7f6376fd7b8f6277
Nikolas Victor Mota <nikolasVictorTola@gmail.com>
2024-05-13 18:50:59 -0300

    Resolvendo problema de CORS

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -2,15 +2,14 @@ var express = require('express')
 var cors = require('cors')
 var app = express()
  
-app.use(cors())
- 
-app.get('/', function (req, res, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
-})
- 
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-})
+app.use((req, res, next) =>{
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+
+  app.use(cors());
+
+  next();
+});
 //-----------------------------
 
 function tempo(request, response) {
