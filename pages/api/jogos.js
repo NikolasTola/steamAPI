6f0a37f4c9d68d08db4492f53eb0b634ec19e4a0
Nikolas <nikolasvictortola@gmail.com>
2024-05-16 15:25:40 -0300

    Debug

--- pages/api/jogos.js
+++ pages/api/jogos.js
@@ -37,14 +37,17 @@ const listaMembros = [
       nome: 'Rúbia',
       id: '76561198318749985'
    }
-]
+];
 
-const id = listaMembros[2].id;
+for (let i = 0; i < listaMembros.length; i++) {
+   const id = listaMembros[i].id;
 
-var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${acessKey}&steamid=${id}&format=json`);
-//var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=2627FEB2EFFCED8437F659EB577291EE&steamid=76561198841564068&format=json`);
+   var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${acessKey}&steamid=${id}&format=json`);
+   //var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=2627FEB2EFFCED8437F659EB577291EE&steamid=76561198841564068&format=json`);
 
-var steamResponseJSON = await steamResponse.json();
+   var steamResponseJSON = await steamResponse.json();
+}
+   
 
 // Resposta da API
 res.json({
