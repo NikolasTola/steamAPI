1a1e203c4eb7b28640742538cde2d1c2371bfdfb
Nikolas <nikolasvictortola@gmail.com>
2024-05-16 18:10:07 -0300

    Debug

--- pages/api/jogos.js
+++ pages/api/jogos.js
@@ -55,8 +55,6 @@ for (let i = 0; i < listaMembros.length; i++) {
 
    let ids = steamResponseJSON.response.games;
 
-   ids = typeof ids;
-
    /*for (let j = 0; j < steamResponseJSON.response.games.length; j++) {
       
       //ids[j] = steamResponseJSON.response.games[j].appid;
