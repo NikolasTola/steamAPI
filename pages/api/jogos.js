6e665cd8c271f15a0ebcb1eccc571c2f4a8f04ed
Nikolas <nikolasvictortola@gmail.com>
2024-05-16 14:52:18 -0300

    Debug

--- pages/api/jogos.js
+++ pages/api/jogos.js
@@ -10,17 +10,21 @@ async function carregaJogos(req, res) {
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 });
 
-//const chaveAcesso = ACCESS_KEY_SECRET;
+const chaveAcesso = ACCESS_KEY_SECRET;
 
-//const id = '76561198841564068';
+const id = '76561198841564068';
 
-//var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${chaveAcesso}&steamid=${id}&format=json`);
+
 var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=2627FEB2EFFCED8437F659EB577291EE&steamid=76561198841564068&format=json`);
 var steamResponseJSON = await steamResponse.json();
 
+var steamResponse2 = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${chaveAcesso}&steamid=${id}&format=json`);
+var steamResponse2JSON = await steamResponse2.json();
+
 // Resposta da API
 res.json({
-      jogos: steamResponseJSON
+      jogos: steamResponseJSON,
+      jogos2: steamResponse2JSON
    });
 }
 
