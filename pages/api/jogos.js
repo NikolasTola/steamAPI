a0fd1a2fb6e023420a503600593e2d8bc5e14712
Nikolas <nikolasvictortola@gmail.com>
2024-05-16 13:25:48 -0300

    Tentando acessar GetOwnedGames

--- pages/api/jogos.js
+++ pages/api/jogos.js
@@ -10,7 +10,7 @@ async function carregaJogos(req, res) {
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 });
 
-   //const accessKey = process.env.ACCESS_KEY_SECRET;
+   const accessKey = process.env.ACCESS_KEY_SECRET;
 
    const listaMembros = [
       {
@@ -45,12 +45,11 @@ async function carregaJogos(req, res) {
   }];
 
  // for (let i = 0; i < listaMembros.length; i++) {
-   //let id = listaMembros[i].id;
-   var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=2627FEB2EFFCED8437F659EB577291EE&steamid=76561198841564068&format=json`);
+   var id = listaMembros[0].id;
+   var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${accessKey}&steamid=${id}&format=json`);
    var steamResponseJSON = await steamResponse.json();
 
    let listaMeusJogos = steamResponseJSON.response.games;
-
 
   // lista_ids[i].nome = listaMembros[i].nome;
 
