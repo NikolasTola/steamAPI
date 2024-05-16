bf5f7b9602860cd496857f3a76dc1b3fe9719ae5
Nikolas <nikolasvictortola@gmail.com>
2024-05-16 14:40:19 -0300

    Chamada do método GetOwnerGames

--- pages/api/jogos.js
+++ pages/api/jogos.js
@@ -9,8 +9,13 @@ async function carregaJogos(req, res) {
       origin: '*',
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 });
-   //var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${accessKey}&steamid=${id}&format=json`);
-   var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=2627FEB2EFFCED8437F659EB577291EE&steamid=76561198841564068&format=json`);
+
+const accessKey = ACCESS_KEY_SECRET;
+
+var id = '76561198841564068';
+
+   var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${accessKey}&steamid=${id}&format=json`);
+   //var steamResponse = await fetch(`http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=2627FEB2EFFCED8437F659EB577291EE&steamid=76561198841564068&format=json`);
    var steamResponseJSON = await steamResponse.json();
 
 // Resposta da API
