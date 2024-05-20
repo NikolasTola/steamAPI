5b1bb494af995ce429e3b3b7a482f71093ddaa6c
Nikolas <nikolasvictortola@gmail.com>
2024-05-20 17:33:46 -0300

    Debug

--- pages/api/jogos.js
+++ pages/api/jogos.js
@@ -108,6 +108,7 @@ for (let i = 0; i < listaMembro_appId.length; i++) {
       //var steamResponse_2 = await fetch(`https://store.steampowered.com/api/appdetails/?appids=${id_api2}`);
       //var steamResponse_2 = await fetch('https://store.steampowered.com/api/appdetails/?appids=359550');
       //var steamResponseJSON_2 = await steamResponse_2.json();
+      console.log(id_api2);
       
    }
 }
