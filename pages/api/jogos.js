e26f7a572ae6e64f9ccb751e5dd0dfa179ebe9d5
Nikolas <nikolasvictortola@gmail.com>
2024-05-20 16:37:26 -0300

    Debug

--- pages/api/jogos.js
+++ pages/api/jogos.js
@@ -85,10 +85,6 @@ for (let i = 0; i < listaMembros.length; i++) {
 
 // SEGUNDA PARTE:
 
-//var steamResponse_2 = await fetch(`https://store.steampowered.com/api/appdetails/?appids=${id_api2}`);
-var steamResponse_2 = await fetch('https://store.steampowered.com/api/appdetails/?appids=359550');
-var steamResponseJSON_2 = await steamResponse_2.json();
-
 var listaJogosOW = [];
 
 var objetoAuxiliar = {
@@ -104,9 +100,9 @@ for (let i = 0; i < listaMembro_appId.length; i++) {
 
       var id_api2 = listaMembro_appId[i].idJogos[j];
 
-      var steamResponse_2 = await fetch(`https://store.steampowered.com/api/appdetails/?appids=${id_api2}`);
+      /*var steamResponse_2 = await fetch(`https://store.steampowered.com/api/appdetails/?appids=${id_api2}`);
       //var steamResponse_2 = await fetch('https://store.steampowered.com/api/appdetails/?appids=359550');
-      var steamResponseJSON_2 = await steamResponse_2.json();
+      var steamResponseJSON_2 = await steamResponse_2.json();*/
       
       
       
